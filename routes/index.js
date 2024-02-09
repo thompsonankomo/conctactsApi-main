00ab1640fee9d3c6e52a51709df729a824b4c5b4
Thompson Nkomo <nko20005@byui.edu>
2024-02-09 04:50:25 +0200

    commit

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const swagger = require('./swagger');
 // Main router middle ware, index route
 router.get('/', (req, res) => {
     //#swagger.tags=['Welcome Message']
-    res.send( "<center><h1 style='color:blue; padding-top: 15rem; font-size:6rem'>Welcome, this is my CSE 341 Contacts Project Home Page: Part-2</h1></center>");
+    res.send( "<center><h1 style='color:Green; padding-top: 10rem; font-size:4rem'>Welcome to my CSE341 Contacts API page</h1></center>");
 })
 
 router.use('/', require('./swagger'));
